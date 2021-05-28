da4bc0e716e185b05fdb3cfcb99021b2c5ba3cdb
sheeshalicia <maenadish@gmail.com>
2021-05-28 16:15:43 -0500

    Added change event listener for selected number of users

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function () {
-    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+const getData = async function (numUsers) {
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
     const data = await usersRequest.json();
 
     const userResults = data.results;
     displayUsers(userResults);
     };
 
-getData();
+getData(1);
 
 const displayUsers = function (userResults) {
 randomFolks.innerHTML = "";
@@ -27,3 +28,9 @@ userDiv.innerHTML = `
 randomFolks.append(userDiv);
 };
 }
+
+selectUserNumber.addEventListener("change", function (e) {
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
+
